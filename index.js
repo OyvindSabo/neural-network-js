34955f555f238e30d344ffd82f38296d2389e370
oyvindsabo <oyvind.sabo@gmail.com>
2019-03-09 17:00:47 +0100

    Create working example which determines wheter the middle element in an array is 1

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const { createNetwork, getOutput, trainNetwork } = require('./network');
 
 const config = {
   // For creating the network
-  inputLength: 2,
+  inputLength: 3,
   hiddenLength: 2,
-  outputLength: 1,
+  outputLength: 0.5,
   // For training the network
   maxWeight: 50,
-  maxError: 0.2,
+  maxError: 0.005,
 };
 
 const trainingData = [
-  {input: [0, 0], output: [0]},
-  {input: [0, 1], output: [1]},
-  {input: [1, 0], output: [1]},
-  {input: [1, 1], output: [0]},
+  {input: [0, 0, 0], output: [0]},
+  {input: [0, 0, 1], output: [0]},
+  {input: [0, 1, 0], output: [1]},
+  //{input: [0, 1, 1], output: [1]},
+  {input: [1, 0, 0], output: [0]},
+  //{input: [1, 0, 1], output: [0]},
+  {input: [1, 1, 0], output: [1]},
+  {input: [1, 1, 1], output: [1]},
 ];
 
 const network = createNetwork(config);
@@ -24,7 +28,7 @@ console.log('network before training: ', network);
 
 trainNetwork(network, trainingData, config);
 console.log('network after training: ', network);
-console.log('getOutput(network, [0, 0]): ', getOutput(network, [0, 0]));
-console.log('getOutput(network, [0, 1]): ', getOutput(network, [0, 1]));
-console.log('getOutput(network, [1, 0]): ', getOutput(network, [1, 0]));
-console.log('getOutput(network, [1, 1]): ', getOutput(network, [1, 1]));
+console.log('getOutput(network, [0, 1, 1]): ', getOutput(network, [0, 1, 1]));
+console.log('getOutput(network, [1, 0, 1]): ', getOutput(network, [1, 0, 1]));
+//console.log('getOutput(network, [1, 1, 0]): ', getOutput(network, [1, 1, 0]));
+//console.log('getOutput(network, [1, 1, 1]): ', getOutput(network, [1, 1, 1]));
